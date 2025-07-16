24b347f13655d1a823adf2b8d27c20f1fd722302
duskfallcrew <kieranxsomer@gmail.com>
2025-07-17 11:55:43 +1200

    Fix FileLoadResult import on Themes-Lost-Dreams branch

--- dataset_tools/ui/main_window.py
+++ dataset_tools/ui/main_window.py
@@ -26,7 +26,7 @@ from ..logger import debug_monitor  # pylint: disable=relative-beyond-top-level
 from ..logger import info_monitor as nfo  # pylint: disable=relative-beyond-top-level
 from ..metadata_parser import parse_metadata  # pylint: disable=relative-beyond-top-level
 from ..widgets import FileLoader  # pylint: disable=relative-beyond-top-level
-from ..widgets import FileLoadResult
+from .widgets import FileLoadResult
 from .dialogs import SettingsDialog
 from .enhanced_theme_manager import get_enhanced_theme_manager  # pylint: disable=relative-beyond-top-level
 from .font_manager import apply_fonts_to_app  # pylint: disable=relative-beyond-top-level
